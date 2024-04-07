e5e14dd7d58847a390231612420b5629768f5a6a
katya melnik <katamelnik159@gmail.com>
2024-04-07 22:14:35 +0500

    добавила убирание оплаты у всех кроме родителя

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -2,9 +2,40 @@
 {
     public partial class AppShell : Shell
     {
+        private int parentSelectionCount = 0;
         public AppShell()
         {
             InitializeComponent();
+
+            WhoAreYou.StudentSelectedChanged += (sender, e) =>
+            {
+                if (WhoAreYou.isStudentSelected)
+                {
+                    Home.Items.Remove(PaymentContent);
+                }
+            };
+            WhoAreYou.TeacherSelectedChanged += (sender, e) =>
+            {
+                if (WhoAreYou.isTeacherSelected)
+                {
+                    Home.Items.Remove(PaymentContent);
+                }
+            };
+            WhoAreYou.ParentSelectedChanged += (sender, e) =>
+            {
+                if (WhoAreYou.isParentSelected)
+                {
+                    Home.Items.Add(PaymentContent);
+                }
+            };
+            WhoAreYou.DirectorSelectedChanged += (sender, e) =>
+            {
+                if (WhoAreYou.isDirectorSelected)
+                {
+                    Home.Items.Remove(PaymentContent);
+                }
+            };
         }
+
     }
 }
--- WhoAreYou.xaml.cs
+++ WhoAreYou.xaml.cs
@@ -2,12 +2,45 @@ namespace wave;
 
 public partial class WhoAreYou : ContentPage
 {
-	public WhoAreYou()
-	{
-		InitializeComponent();
-	}
+    public static bool isStudentSelected;
+    public static bool isTeacherSelected;
+    public static bool isParentSelected;
+    public static bool isDirectorSelected;
+    public static event EventHandler StudentSelectedChanged;
+    public static event EventHandler TeacherSelectedChanged;
+    public static event EventHandler ParentSelectedChanged;
+    public static event EventHandler DirectorSelectedChanged;
+
+    public WhoAreYou()
+    {
+        InitializeComponent();
+    }
     private async void StudentButtonClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//Authorization");
+        isStudentSelected = true;
+
+        StudentSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+    private async void TeacherButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Authorization");
+        isTeacherSelected = true;
+
+        TeacherSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+    private async void ParentButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Authorization");
+        isParentSelected = true;
+
+        ParentSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+    private async void DirectorButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Authorization");
+        isDirectorSelected = true;
+
+        DirectorSelectedChanged?.Invoke(this, EventArgs.Empty);
     }
 }
