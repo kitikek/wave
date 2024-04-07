aa5f306fe7e96282be06a3c6c5cb066c854485b4
katya melnik <katamelnik159@gmail.com>
2024-04-07 14:15:54 +0500

    + страница выбора кто ты есть

---
+++ Payment.xaml.cs
@@ -0,0 +1,9 @@
+namespace wave;
+
+public partial class Payment : ContentPage
+{
+	public Payment()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WhoAreYou.xaml.cs
@@ -0,0 +1,9 @@
+namespace wave;
+
+public partial class WhoAreYou : ContentPage
+{
+	public WhoAreYou()
+	{
+		InitializeComponent();
+	}
+}
