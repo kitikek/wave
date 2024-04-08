2e2bdffc5df2d7a85a67a9f9bdd7c2db6b42ca0a
Эдуард <BezvodEd@yandex.ru>
2024-04-08 21:07:45 +0500

    посещаемость добавил конкретный урок

--- Attendance.xaml.cs
+++ Attendance.xaml.cs
@@ -23,15 +23,17 @@ public partial class Attendance : ContentPage
         {
             con.Open();
 
-            string sql = "SELECT lesson_visit.lesson_visit_date, groups.group_name, lesson_visit.lesson_visit " +
+            string sql = "SELECT lesson_exact.lesson_exact_data, groups.group_name, lesson_visit.lesson_visit " +
                 "FROM lesson_visit " +
                 "JOIN student " +
                 "ON student.student_id=1 AND student.student_id=lesson_visit.lesson_visit_student_id " +
+                "JOIN lesson_exact " +
+                "ON lesson_visit.lesson_visit_lesson_exact_id=lesson_exact.lesson_exact_id " +
                 "JOIN lesson " +
-                "ON lesson_visit.lesson_visit_lesson_id=lesson.lesson_id " +
+                "ON lesson_exact.lesson_exact_lesson_id=lesson.lesson_id " +
                 "JOIN groups " +
                 "ON lesson.lesson_group_id=groups.group_id " +
-                "ORDER BY lesson_visit.lesson_visit_date DESC;";
+                "ORDER BY lesson_exact.lesson_exact_data DESC;";
             MySqlCommand cmd = new MySqlCommand(sql, con);
             MySqlDataReader reader = cmd.ExecuteReader();
 
