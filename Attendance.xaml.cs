e03a135ddb9b8d7fc80e503cdb5c6b81c321ac61
Эдуард <BezvodEd@yandex.ru>
2024-04-08 17:12:10 +0500

    посещаемость добавлена

--- Attendance.xaml.cs
+++ Attendance.xaml.cs
@@ -1,9 +1,52 @@
+using MySqlConnector;
+using System.Globalization;
+
 namespace wave;
+
+public class AttendanceItem
+{
+    public string? Date { get; set; }
+    public string? Group { get; set; }
+    public string? Visit { get; set; }
+}
 
 public partial class Attendance : ContentPage
 {
-	public Attendance()
-	{
-		InitializeComponent();
-	}
+    public Attendance()
+    {
+        InitializeComponent();
+
+        var items = new List<AttendanceItem>();
+        var cs = ConnString.connString;
+
+        using (var con = new MySqlConnection(cs))
+        {
+            con.Open();
+
+            string sql = "SELECT lesson_visit.lesson_visit_date, groups.group_name, lesson_visit.lesson_visit " +
+                "FROM lesson_visit " +
+                "JOIN student " +
+                "ON student.student_id=1 AND student.student_id=lesson_visit.lesson_visit_student_id " +
+                "JOIN lesson " +
+                "ON lesson_visit.lesson_visit_lesson_id=lesson.lesson_id " +
+                "JOIN groups " +
+                "ON lesson.lesson_group_id=groups.group_id " +
+                "ORDER BY lesson_visit.lesson_visit_date DESC;";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string formattedDate = reader.GetDateTime(0).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+                items.Add(new AttendanceItem { Date = formattedDate, Group = reader.GetString(1), Visit = reader.GetString(2) });
+            }
+        }
+
+        if (items.Count == 0)
+        {
+            items.Add(new AttendanceItem { Group = "ќценок пока нет" });
+        };
+
+        attendanceList.ItemsSource = items;
+    }
 }
--- Grade.xaml.cs
+++ Grade.xaml.cs
@@ -6,9 +6,9 @@ namespace wave;
 
 public class GradeItem
 {
-    public string Name { get; set; }
-    public string Date { get; set; }
-    public string Grade { get; set; }
+    public string? Name { get; set; }
+    public string? Date { get; set; }
+    public string? Grade { get; set; }
 }
 
 public partial class Grade : ContentPage
@@ -24,20 +24,23 @@ public partial class Grade : ContentPage
         {
             con.Open();
 
-            string sql = "SELECT test.test_name, test.test_date, test_visit.test_visit_mark FROM test, test_visit WHERE test_visit.test_visit_student_id=1 AND test_visit.test_visit_test_id=test.test_id ORDER BY test.test_date DESC;";
+            string sql = "SELECT test.test_name, test.test_date, test_visit.test_visit_mark, test_visit.test_visit " +
+                "FROM test, test_visit " +
+                "WHERE test_visit.test_visit_student_id=1 AND test_visit.test_visit_test_id=test.test_id " +
+                "ORDER BY test.test_date DESC;";
             MySqlCommand cmd = new MySqlCommand(sql, con);
             MySqlDataReader reader = cmd.ExecuteReader();
 
             while (reader.Read())
             {
                 string formattedDate = reader.GetDateTime(1).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
-                items.Add(new GradeItem { Name = reader.GetString(0), Date = formattedDate, Grade = reader.GetString(2) });
+                items.Add(new GradeItem { Name = reader.GetString(0) + $" ({reader.GetString(3)})", Date = formattedDate, Grade = reader.GetString(2) });
             }
         }
 
         if (items.Count == 0)
         {
-            items.Add(new GradeItem { Name = "ќценок пока нет"});
+            items.Add(new GradeItem { Name = "ќценок пока нет" });
         };
 
         gradeList.ItemsSource = items;
