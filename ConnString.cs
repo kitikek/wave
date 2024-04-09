0f539396b35b9b88a44ba5d40a71a2470c30ac48
vladislavaskachko <ms.vskachko@yandex.ru>
2024-04-09 23:55:19 +0500

    конфликт

--- ConnString.cs
+++ ConnString.cs
@@ -8,10 +8,7 @@ namespace wave
 {
     public class ConnString
     {
-<<<<<<< HEAD
+
         public static string connString = "Server = 192.168.5.171; User ID = Edward; Database = project";
-=======
-        public static string connString = "Server = 192.168.5.171; User ID = Edward; Database = project";
->>>>>>> 1d0b6abe139a89a3fe1d4469ad8ef1b679ecec1f
     }
 }
