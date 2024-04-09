b7d769774fe5be2e3ab412aa95a1db26c397837b
Эдуард <BezvodEd@yandex.ru>
2024-04-09 21:45:58 +0500

    убогое изменение добавлено

--- AttendanceTeacher.xaml.cs
+++ AttendanceTeacher.xaml.cs
@@ -15,10 +15,9 @@ namespace wave
             {
                 con.Open();
 
-                // Запросы для получения данных
+                // Получение данных для всплывающего списка групп
                 string groupQuery = "SELECT group_name FROM groups ORDER BY group_name ASC;";
 
-                // Получение данных для всплывающего списка
                 var groups = new List<string>();
                 using (var cmd = new MySqlCommand(groupQuery, con))
                 using (var reader = cmd.ExecuteReader())
@@ -31,8 +30,7 @@ namespace wave
                 GroupPicker.ItemsSource = groups; // Заполнение всплывающего списка
             }
 
-            string groupName = "АЯ ПК 3";
-            Load(groupName);
+            string groupName = "";
             GroupPicker.SelectedIndexChanged += (sender, e) =>
             {
                 string selectedGroup = GroupPicker.SelectedItem?.ToString();
@@ -54,11 +52,12 @@ namespace wave
             {
                 con.Open();
 
+                //запросы
                 string rowCountQuery = $"SELECT COUNT(*) FROM students_groups, groups WHERE groups.group_name=@GroupName AND groups.group_id=students_groups.group_id;";
                 string columnCountQuery = $"SELECT COUNT(*) FROM groups JOIN lesson ON groups.group_name=@GroupName AND groups.group_id=lesson.lesson_group_id JOIN lesson_exact ON lesson.lesson_id=lesson_exact.lesson_exact_lesson_id;";
-                string studentNamesQuery = $"SELECT student_id FROM students_groups, groups WHERE groups.group_name=@GroupName AND students_groups.group_id=groups.group_id GROUP BY student_id ASC;";
+                string studentNamesQuery = $"SELECT CONCAT(users.user_surname, ' ', users.user_name) FROM students_groups JOIN groups ON students_groups.group_id=groups.group_id AND groups.group_name=@GroupName JOIN student ON student.student_id=students_groups.student_id JOIN users ON users.user_id=student.student_user_id GROUP BY users.user_surname ASC;;";
                 string dateNamesQuery = $"SELECT lesson_exact_data FROM groups JOIN lesson ON groups.group_name=@GroupName AND groups.group_id=lesson.lesson_group_id JOIN lesson_exact ON lesson.lesson_id=lesson_exact.lesson_exact_lesson_id GROUP BY lesson_exact.lesson_exact_data ASC;";
-                string attendanceQuery = $"SELECT lesson_visit FROM groups JOIN lesson ON lesson.lesson_group_id=groups.group_id AND groups.group_name=@GroupName JOIN lesson_exact ON lesson_exact.lesson_exact_lesson_id=lesson.lesson_id JOIN lesson_visit ON lesson_visit.lesson_visit_lesson_exact_id=lesson_exact.lesson_exact_id ORDER BY lesson_visit.lesson_visit_student_id ASC, lesson_exact.lesson_exact_data ASC;";
+                string attendanceQuery = $"SELECT lesson_visit FROM groups JOIN lesson ON lesson.lesson_group_id=groups.group_id AND groups.group_name=@GroupName JOIN lesson_exact ON lesson_exact.lesson_exact_lesson_id=lesson.lesson_id JOIN lesson_visit ON lesson_visit.lesson_visit_lesson_exact_id=lesson_exact.lesson_exact_id JOIN student ON student.student_id=lesson_visit.lesson_visit_student_id JOIN users ON users.user_id=student.student_user_id ORDER BY users.user_surname ASC, lesson_exact.lesson_exact_data ASC;";
 
                 // Получение количества строк и столбцов
                 int rowCount, columnCount;
@@ -73,7 +72,7 @@ namespace wave
                     columnCount = Convert.ToInt32(cmd.ExecuteScalar()); // Получение количества столбцов
                 }
 
-                // Создание таблицы с именами студентов и дат
+                //Получение имен студентов
                 var studentNames = new List<string>();
                 using (var cmd = new MySqlCommand(studentNamesQuery, con))
                 {
@@ -82,11 +81,12 @@ namespace wave
                     {
                         while (reader.Read())
                         {
-                            studentNames.Add(reader["student_id"].ToString());
+                            studentNames.Add(reader.GetString(0).ToString());
                         }
                     }
                 }
 
+                //Получение имен дат
                 var dateNames = new List<string>();
                 using (var cmd = new MySqlCommand(dateNamesQuery, con))
                 {
@@ -100,12 +100,24 @@ namespace wave
                     }
                 }
 
+                //Заполнение первой ячейки
+                Label studentLabel1 = new Label
+                {
+                    Text = @"Ученики\Даты",
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                };
+                Grid.SetRow(studentLabel1, 0);
+                Grid.SetColumn(studentLabel1, 0);
+                AttendanceGrid.Children.Add(studentLabel1);
+
+                //заполнение студентов
                 for (int i = 0; i < rowCount; i++)
                 {
                     Label studentLabel = new Label
                     {
                         Text = studentNames[i],
-                        HorizontalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Start,
                         VerticalOptions = LayoutOptions.Center
                     };
                     Grid.SetRow(studentLabel, i + 1);
@@ -113,6 +125,7 @@ namespace wave
                     AttendanceGrid.Children.Add(studentLabel);
                 }
 
+                //заполнение дат
                 for (int j = 0; j < columnCount; j++)
                 {
                     Label dateLabel = new Label
@@ -126,6 +139,7 @@ namespace wave
                     AttendanceGrid.Children.Add(dateLabel);
                 }
 
+                //Получение посещаемости
                 var attendances = new List<string>();
                 using (var cmd = new MySqlCommand(attendanceQuery, con))
                 {
@@ -139,7 +153,7 @@ namespace wave
                     }
                 }
 
-                // Пример заполнения ячеек таблицы
+                //Заполнение ячеек посещаемости
                 for (int i = 0; i < rowCount; i++)
                 {
                     for (int j = 0; j < columnCount; j++)
@@ -156,7 +170,7 @@ namespace wave
                     }
                 }
                 AttendanceGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-                for (int j = 0; j < columnCount; j++)
+                for (int j = 1; j < columnCount + 1; j++)
                 {
                     AttendanceGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
                 }
