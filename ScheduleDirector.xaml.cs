081d3fc69e8a5846ee3774a776eed7884bca2ace
katya melnik <katamelnik159@gmail.com>
2024-04-10 20:24:21 +0500

    недоделанное расписание  + размеры кнопок

--- ScheduleDirector.xaml.cs
+++ ScheduleDirector.xaml.cs
@@ -1,3 +1,4 @@
+using MySqlConnector;
 using System;
 using System.Collections.ObjectModel;
 
@@ -5,26 +6,68 @@ namespace wave;
 
 public partial class ScheduleDirector : ContentPage
 {
-    static Random random = new();
-    public ObservableCollection<Student> Items { get; } = new();
+    private ObservableCollection<Lesson> LessonsList = new ObservableCollection<Lesson>();
     public ScheduleDirector()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
 
-        for (int i = 0; i < 10; i++)
+        var cs = ConnString.connString;
+
+        using (var con = new MySqlConnection(cs))
         {
-            Items.Add(new Student
+            con.Open();
+            var cmd = new MySqlCommand("SELECT l.lesson_id, l.lesson_day_id, CONCAT(l.lesson_start, ' - ', l.lesson_end) AS lessonTime, g.group_name AS groupName, r.room_name AS roomName, CONCAT(LEFT(u.user_name, 1), '.', LEFT(u.user_patronymic, 1), '.') AS teacherInitials FROM lesson l JOIN groups g ON l.lesson_group_id = g.group_id JOIN room r ON l.lesson_room_id = r.room_id JOIN teacher t ON g.group_teacher_id = t.teacher_id JOIN users u ON t.teacher_user_id = u.user_id;", con);
+
+            using (var dr = cmd.ExecuteReader())
             {
-                Id = i,
-                Name = "Person " + i,
-                Age = random.Next(14, 85),
-            });
+                while (dr.Read())
+                {
+                    var lessonId = (int)dr["lesson_id"];
+                    var day = (int)dr["lesson_day_id"];
+                    var room = dr["roomName"].ToString();
+                    var lessonTime = dr["lessonTime"].ToString();
+                    var groupName = dr["groupName"].ToString();
+                    var teacher = dr["teacherInitials"].ToString(); 
+
+                    LessonsList.Add(new Lesson() { LessonName = lessonId, Day = day, Room = room, Str = $"{lessonTime}  {groupName}  {teacher}" });
+                }
+            }
+        }
+        UpdateUIBasedOnConditions();
+    }
+    private void UpdateUIBasedOnConditions()
+    {
+        foreach (var lesson in LessonsList)
+        {
+            if (lesson.Room == "Англия" && lesson.Day == 1)
+            {
+                if (MondayLabel.Text != null)
+                    MondayLabel.Text += $"{Environment.NewLine}{lesson.Str}";
+                else
+                    MondayLabel.Text = lesson.Str;
+            }
+            if (lesson.Room == "Англия" && lesson.Day == 2)
+            {
+                if (TuesdayLabel.Text != null)
+                    TuesdayLabel.Text += $"{Environment.NewLine}{lesson.Str}";
+                else
+                    TuesdayLabel.Text = lesson.Str;
+            }
+            if (lesson.Room == "Англия" && lesson.Day == 3)
+            {
+                if (WednesdayLabel.Text != null)
+                    WednesdayLabel.Text += $"{Environment.NewLine}{lesson.Str}";
+                else
+                    WednesdayLabel.Text = lesson.Str;
+            }
         }
     }
-    public class Student
+
+    public class Lesson
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public int LessonName { get; set; }
+        public int Day { get; set; }
+        public string Room { get; set; }
+        public string Str { get; set; }
     }
 }
