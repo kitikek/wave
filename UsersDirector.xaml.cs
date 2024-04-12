5e8611516ff72b6c0581ee3796a7564d72c6cd17
Эдуард <BezvodEd@yandex.ru>
2024-04-12 17:50:56 +0500

    жуть

--- UsersDirector.xaml.cs
+++ UsersDirector.xaml.cs
@@ -1,9 +1,24 @@
-namespace wave;
-
-public partial class UsersDirector : ContentPage
+namespace wave
 {
-	public UsersDirector()
-	{
-		InitializeComponent();
-	}
+    public partial class UsersDirector : ContentPage
+    {
+        
+        public UsersDirector()
+        {
+            InitializeComponent();
+
+            txtHours.TextChanged += OnEntryTextChanged;
+            txtHoursCost.TextChanged += OnEntryTextChanged;
+            void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+            {
+                if (!string.IsNullOrEmpty(e.NewTextValue))
+                {
+                    if (!int.TryParse(e.NewTextValue, out _))
+                    {
+                        ((Entry)sender).Text = e.OldTextValue;
+                    }
+                }
+            }
+        }
+    }
 }
