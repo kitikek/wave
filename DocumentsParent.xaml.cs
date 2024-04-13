8928fa7ef4d5eff0b39caa0d0a5924b2523006c3
vladislavaskachko <ms.vskachko@yandex.ru>
2024-04-13 23:02:10 +0500

    Фикс отображения кнопок + Документы у родителя

--- DocumentsParent.xaml.cs
+++ DocumentsParent.xaml.cs
@@ -1,9 +1,44 @@
-namespace wave;
+using Microsoft.Maui.Controls;
 
-public partial class DocumentsParent : ContentPage
+namespace wave
 {
-	public DocumentsParent()
-	{
-		InitializeComponent();
-	}
+    public partial class DocumentsParent : ContentPage
+    {
+        public DocumentsParent()
+        {
+            InitializeComponent();
+            AddOpenButton();
+        }
+
+        private void AddOpenButton()
+        {
+            StackLayout stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Spacing = 10 
+            };
+
+            Label label = new Label
+            {
+                Text = "Чтобы открыть папку с документами, нажмите на кнопку",
+                HorizontalOptions = LayoutOptions.Center
+            };
+            stackLayout.Children.Add(label);
+
+            Button openButton = new Button
+            {
+                Text = "Открыть",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            openButton.Clicked += async (sender, args) =>
+            {
+                await Launcher.OpenAsync("https://drive.google.com/drive/folders/18El5UBnRANgIMfvuoymusHMervVa-DNK?usp=sharing");
+            };
+            stackLayout.Children.Add(openButton);
+
+            Content = stackLayout;
+        }
+    }
 }
