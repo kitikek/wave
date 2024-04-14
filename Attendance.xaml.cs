71d29a277df5ff3a959246f54ec9da358966fbe5
Эдуард <BezvodEd@yandex.ru>
2024-04-14 22:05:44 +0500

    договор + ученик конкретный

--- Attendance.xaml.cs
+++ Attendance.xaml.cs
@@ -1,5 +1,7 @@
+using CommunityToolkit.Maui.Converters;
 using MySqlConnector;
 using System.Globalization;
+using System.Reflection.PortableExecutable;
 
 namespace wave;
 
@@ -16,17 +18,36 @@ public partial class Attendance : ContentPage
     {
         InitializeComponent();
 
-        var items = new List<AttendanceItem>();
+        var items = new List<AttendanceItem> { new AttendanceItem { Date = "Дата:", Group = "Группа:", Visit = "Оценка:" } };
         var cs = ConnString.connString;
+        int id = 0;
 
         using (var con = new MySqlConnection(cs))
         {
             con.Open();
+            using (MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*), student_id FROM student JOIN users ON student_user_id=user_id AND user_login=@l AND user_password=@p", con))
+            {
+                cmd.Parameters.AddWithValue("@l", Authorization.Login);
+                cmd.Parameters.AddWithValue("@p", Authorization.Password);
+                MySqlDataReader reader = cmd.ExecuteReader();
 
+                while (reader.Read())
+                {
+                    if (reader.GetInt32(0) > 0)
+                    {
+                        id = reader.GetInt32(1);
+                    }
+                }
+            }
+        }
+
+        using (var con = new MySqlConnection(cs))
+        {
+            con.Open();
             string sql = "SELECT lesson_exact.lesson_exact_data, groups.group_name, lesson_visit.lesson_visit " +
                 "FROM lesson_visit " +
                 "JOIN student " +
-                "ON student.student_id=1 AND student.student_id=lesson_visit.lesson_visit_student_id " +
+                "ON student.student_id=@id AND student.student_id=lesson_visit.lesson_visit_student_id " +
                 "JOIN lesson_exact " +
                 "ON lesson_visit.lesson_visit_lesson_exact_id=lesson_exact.lesson_exact_id " +
                 "JOIN lesson " +
@@ -34,19 +55,22 @@ public partial class Attendance : ContentPage
                 "JOIN groups " +
                 "ON lesson.lesson_group_id=groups.group_id " +
                 "ORDER BY lesson_exact.lesson_exact_data DESC;";
-            MySqlCommand cmd = new MySqlCommand(sql, con);
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
+            using (MySqlCommand cmd = new MySqlCommand(sql, con))
             {
-                string formattedDate = reader.GetDateTime(0).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
-                items.Add(new AttendanceItem { Date = formattedDate, Group = reader.GetString(1), Visit = reader.GetString(2) });
+                cmd.Parameters.AddWithValue("@id", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string formattedDate = reader.GetDateTime(0).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+                    items.Add(new AttendanceItem { Date = formattedDate, Group = reader.GetString(1), Visit = reader.GetString(2) });
+                }
             }
         }
 
-        if (items.Count == 0)
+        if (items.Count == 1)
         {
-            items.Add(new AttendanceItem { Group = "Оценок пока нет" });
+            items.Add(new AttendanceItem { Date = "Оценок пока нет", Group="", Visit="" });
         };
 
         attendanceList.ItemsSource = items;
--- Grade.xaml.cs
+++ Grade.xaml.cs
@@ -17,30 +17,52 @@ public partial class Grade : ContentPage
     {
         InitializeComponent();
 
-        var items = new List<GradeItem>();
+        var items = new List<GradeItem> { new GradeItem { Name = "Тест:", Date = "Дата:", Grade = "Оценка:" } };
         var cs = ConnString.connString;
+        int id = 0;
 
         using (var con = new MySqlConnection(cs))
         {
             con.Open();
-
-            string sql = "SELECT test.test_name, test.test_date, test_visit.test_visit_mark " +
-                "FROM test, test_visit " +
-                "WHERE test_visit.test_visit_student_id=1 AND test_visit.test_visit_test_id=test.test_id " +
-                "ORDER BY test.test_date DESC;";
-            MySqlCommand cmd = new MySqlCommand(sql, con);
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
+            using (MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*), student_id FROM student JOIN users ON student_user_id=user_id AND user_login=@l AND user_password=@p", con))
             {
-                string formattedDate = reader.GetDateTime(1).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
-                items.Add(new GradeItem { Name = reader.GetString(0), Date = formattedDate, Grade = reader.GetString(2) });
+                cmd.Parameters.AddWithValue("@l", Authorization.Login);
+                cmd.Parameters.AddWithValue("@p", Authorization.Password);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (reader.GetInt32(0) > 0)
+                    {
+                        id = reader.GetInt32(1);
+                    }
+                }
             }
         }
 
-        if (items.Count == 0)
+        using (var con = new MySqlConnection(cs))
         {
-            items.Add(new GradeItem { Name = "Оценок пока нет" });
+            con.Open();
+            string sql = "SELECT test.test_name, test.test_date, test_visit.test_visit_mark " +
+                "FROM test, test_visit " +
+                "WHERE test_visit.test_visit_student_id=@id AND test_visit.test_visit_test_id=test.test_id " +
+                "ORDER BY test.test_date DESC;";
+            using (MySqlCommand cmd = new MySqlCommand(sql, con))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string formattedDate = reader.GetDateTime(1).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+                    items.Add(new GradeItem { Name = reader.GetString(0), Date = formattedDate, Grade = reader.GetString(2) });
+                }
+            }
+        }
+
+        if (items.Count == 1)
+        {
+            items.Add(new GradeItem { Name = "Оценок пока нет", Date = "Никогда", Grade = "Ничего" });
         };
 
         gradeList.ItemsSource = items;
