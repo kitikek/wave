1e1c7901b3881355a82857a0785bf2674de9d7d3
katya melnik <katamelnik159@gmail.com>
2024-04-14 11:36:25 +0500

    добавила минимальный размер окна

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace wave
+﻿
+namespace wave
 {
     public partial class App : Application
     {
@@ -7,6 +8,21 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.X = 140;
+            window.Y = 20;
+
+            window.Width = 1300;
+            window.Height = 800;
+
+            window.MinimumWidth = 1300;
+            window.MinimumHeight = 800;
+
+            return window;
         }
     }
 }
